9db8476c8814a1568200c80334b7479044540a43
yeriahn <yeri01080108@gmail.com>
2020-10-20 15:46:16 +0900

    init

---
+++ src/main/java/com/godcoder/thymeleafweb/ThymeleafwebApplication.java
@@ -0,0 +1,13 @@
+package com.godcoder.thymeleafweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/godcoder/thymeleafweb/ThymeleafwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godcoder.thymeleafweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
