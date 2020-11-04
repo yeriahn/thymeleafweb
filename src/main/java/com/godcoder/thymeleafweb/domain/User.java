e0631897327183db601af995333688ce134e8aed
yeriahn <yeri01080108@gmail.com>
2020-11-04 11:01:10 +0900

    spring security 추가하여 로그인, 회원가입 기능 구성

---
+++ src/main/java/com/godcoder/thymeleafweb/domain/User.java
@@ -0,0 +1,28 @@
+package com.godcoder.thymeleafweb.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private Boolean enabled;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles = new ArrayList<>();
+}
---
+++ src/main/java/com/godcoder/thymeleafweb/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.godcoder.thymeleafweb.repository;
+
+import com.godcoder.thymeleafweb.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
